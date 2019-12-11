10b72f2c1f7fb613e30c2923060001301895c7d8
Sagar Talla <talla.sagar@hotmail.com>
2019-12-11 17:07:51 +0530

    box styles

--- packages/common/src/util.ts
+++ packages/common/src/util.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 interface Props {
   flexDirection?: string;
@@ -21,28 +21,29 @@ interface Props {
 }
 
 export const Box = styled.div<Props>`
-  width: ${props => (props.width ? props.width : '100%')};
+  width: ${props => (props.width ? props.width : "100%")};
   ${props =>
-    props.justifyContent ? `justify-content: ${props.justifyContent}` : ''};
-  ${props => (props.alignItems ? `align-items: ${props.alignItems}` : '')};
-  ${props => (props.textAlign ? `text-align: ${props.textAlign}` : '')};
-  ${props => (props.margin ? `margin: ${props.margin}` : '')};
-  ${props => (props.padding ? `padding: ${props.padding}` : '')};
-  ${props => (props.mt ? `margin-top: ${props.mt}` : '')};
-  ${props => (props.mr ? `margin-right: ${props.mr}` : '')};
-  ${props => (props.mb ? `margin-bottom: ${props.mb}` : '')};
-  ${props => (props.ml ? `margin-left: ${props.ml}` : '')};
-  ${props => (props.pt ? `padding-top: ${props.pt}` : '')};
-  ${props => (props.pb ? `padding-bottom: ${props.pb}` : '')};
-  ${props => (props.pl ? `padding-left: ${props.pl}` : '')};
-  ${props => (props.pr ? `padding-right: ${props.pr}` : '')};
-  ${props => (props.overflow ? `overflow: ${props.overflow}` : '')};
+    props.justifyContent ? `justify-content: ${props.justifyContent}` : ""};
+  ${props => (props.alignItems ? `align-items: ${props.alignItems}` : "")};
+  ${props => (props.textAlign ? `text-align: ${props.textAlign}` : "")};
+  ${props => (props.margin ? `margin: ${props.margin}` : "")};
+  ${props => (props.padding ? `padding: ${props.padding}` : "")};
+  ${props => (props.mt ? `margin-top: ${props.mt}` : "")};
+  ${props => (props.mr ? `margin-right: ${props.mr}` : "")};
+  ${props => (props.mb ? `margin-bottom: ${props.mb}` : "")};
+  ${props => (props.ml ? `margin-left: ${props.ml}` : "")};
+  ${props => (props.pt ? `padding-top: ${props.pt}` : "")};
+  ${props => (props.pb ? `padding-bottom: ${props.pb}` : "")};
+  ${props => (props.pl ? `padding-left: ${props.pl}` : "")};
+  ${props => (props.pr ? `padding-right: ${props.pr}` : "")};
+  ${props => (props.overflow ? `overflow: ${props.overflow}` : "")};
+  ${props => props.styles};
 `;
 
 export const Flex = styled(Box)<Props>`
   display: flex;
   ${props =>
-    props.flexDirection ? `flex-direction: ${props.flexDirection}` : ''};
+    props.flexDirection ? `flex-direction: ${props.flexDirection}` : ""};
   ${props => props.styles};
 `;
 
